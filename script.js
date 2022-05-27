6da97216a419edd1bddb59690ce602b1e64bd2d4
rajat153 <rajat.force1@gmail.com>
2022-05-27 09:16:44 +0530

    Intial commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const btn = document.getElementById("btn");
+const input = document.getElementById("city");
+const form = document.getElementById("form");
+
+
+// form.addEventListener("click", async function(e){
+//     try{
+//     e.preventDefault()
+//     //console.log("acaca")
+//     const searchTerm =  input.value;
+//     //console.log(searchTerm);
+//     const config = { params: { q: searchTerm }} ;
+//     const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?appid=07096c8c8e490299cc105549c064c4a3`, config);
+//     //console.log(res.data);
+//     //console.log(res.data.main.temp)
+//     document.body.style.flexDirection="column"
+//     const div = document.createElement('DIV');
+//     div.style.backgroundColor="#fde4cf"
+//     div.style.height="100px"
+//     div.style.marginTop="5px"
+//     div.innerHTML = `Temperature of ${searchTerm} is ${Math.floor(res.data.main.temp-273.15)}°C`
+//     console.log(div.innerText)
+//     document.body.append(div);
+//     input.value = "";
+//     }
+//     catch(e){
+//         console.log("something is wrong",e)
+//     }
+    
+
+
+// })
+
+form.addEventListener("click", async function(e){
+    try{
+    e.preventDefault()
+    //console.log("acaca")
+    const searchTerm = form.elements.query.value;
+    console.log(searchTerm);
+    const config = { params:{ q:searchTerm,appid:'07096c8c8e490299cc105549c064c4a3'} } ;
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather`,config);
+    console.log(res.data);
+    document.body.style.flexDirection="column"
+    const div = document.createElement('DIV');
+    div.style.backgroundColor="#fde4cf"
+    div.style.minWidth="40%"
+    div.style.fontWeight="bold"
+    div.style.marginTop="5px"
+    div.style.padding="2px"
+    div.style.borderRadius="5px"
+    div.innerHTML = `The weather in ${searchTerm} is '${res.data.weather[0].main}' currently.The temperature is ${Math.floor(res.data.main.temp-273.15)}°C and the wind speed is ${res.data.wind.speed}m/s.`
+    document.body.append(div);
+    input.value = "";
+    }
+    catch(e){
+        console.log("something is wrong",e)
+    }
+    
+})
+
+
+
+
