1d55e13978fe605b494020cc61880aff4f540f16
rajat153 <rajat.force1@gmail.com>
2022-10-27 17:38:32 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("city");
 const form = document.getElementById("form");
+const div = document.getElementById("weatherResult")
 
 
 // form.addEventListener("click", async function(e){
@@ -26,22 +27,20 @@ const form = document.getElementById("form");
 //     catch(e){
 //         console.log("something is wrong",e)
 //     }
-    
-
-
 // })
 
-form.addEventListener("click", async function(e){
+form.addEventListener("submit",async function(e){
     try{
+        
     e.preventDefault()
-    //console.log("acaca")
     const searchTerm = form.elements.query.value;
+    if(searchTerm.length!==0){
     console.log(searchTerm);
     const config = { params:{ q:searchTerm,appid:'07096c8c8e490299cc105549c064c4a3'} } ;
     const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather`,config);
     console.log(res.data);
+    div.innerHTML = ""
     document.body.style.flexDirection="column"
-    const div = document.createElement('DIV');
     div.style.backgroundColor="#fde4cf"
     div.style.minWidth="40%"
     div.style.fontWeight="bold"
@@ -50,10 +49,24 @@ form.addEventListener("click", async function(e){
     div.style.borderRadius="5px"
     div.innerHTML = `The weather in ${searchTerm} is '${res.data.weather[0].main}' currently.The temperature is ${Math.floor(res.data.main.temp-273.15)}°C and the wind speed is ${res.data.wind.speed}m/s.`
     document.body.append(div);
-    input.value = "";
+    input.value = ""; 
+    }else{
+        alert("Location cannot be empty")
+    }
     }
     catch(e){
-        console.log("something is wrong",e)
+    console.log("something is wrong",e)
+    div.innerHTML = ""
+    document.body.style.flexDirection="column"
+    div.style.backgroundColor="#fde4cf"
+    div.style.minWidth="40%"
+    div.style.fontWeight="bold"
+    div.style.marginTop="5px"
+    div.style.padding="2px"
+    div.style.borderRadius="5px"
+    div.innerHTML=`The current city is not found`;
+    document.body.append(div);
+    input.value = "";
     }
     
 })
